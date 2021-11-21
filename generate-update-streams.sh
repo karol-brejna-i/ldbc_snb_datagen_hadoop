2d33d4512b047ce98c454f3167fcee9222ecc8a5
Gabor Szarnyas <szarnyasg@gmail.com>
2021-11-21 13:11:22 +0100

    Add script to generate update streams

--- generate-update-streams.sh
+++ generate-update-streams.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-export HADOOP_CLIENT_OPTS="-Xmx20G"
+set -eu
 
-for SF in 0.1 0.3 1 3 10; do
-    echo SF: $SF
+rm -rf social_network/
+rm -f datagen.log
 
-    echo > params.ini
-    echo ldbc.snb.datagen.generator.scaleFactor:snb.interactive.$SF >> params.ini
-    echo ldbc.snb.datagen.serializer.numUpdatePartitions:16 >> params.ini
-    echo ldbc.snb.datagen.serializer.dynamicActivitySerializer:ldbc.snb.datagen.serializer.snb.csv.dynamicserializer.activity.CsvBasicDynamicActivitySerializer >> params.ini
-    echo ldbc.snb.datagen.serializer.dynamicPersonSerializer:ldbc.snb.datagen.serializer.snb.csv.dynamicserializer.person.CsvBasicDynamicPersonSerializer >> params.ini
-    echo ldbc.snb.datagen.serializer.staticSerializer:ldbc.snb.datagen.serializer.snb.csv.staticserializer.CsvBasicStaticSerializer >> params.ini
+for SF in 0.1; do
+    echo "=> SF: ${SF}" | tee -a datagen.log
 
-    ./run.sh
-    mv social_network ../datagen-graphs/social_network-sf$SF
+    for NUMPART in 1 2 4 8 16 32 64 128 256 512; do
+        echo "--> NUMPART: ${NUMPART}" | tee -a datagen.log
+
+        echo > params.ini
+        echo ldbc.snb.datagen.generator.scaleFactor:snb.interactive.${SF} >> params.ini
+        echo ldbc.snb.datagen.serializer.numUpdatePartitions:${NUMPART} >> params.ini
+        echo ldbc.snb.datagen.serializer.dynamicActivitySerializer:ldbc.snb.datagen.serializer.snb.csv.dynamicserializer.activity.CsvBasicDynamicActivitySerializer >> params.ini
+        echo ldbc.snb.datagen.serializer.dynamicPersonSerializer:ldbc.snb.datagen.serializer.snb.csv.dynamicserializer.person.CsvBasicDynamicPersonSerializer >> params.ini
+        echo ldbc.snb.datagen.serializer.staticSerializer:ldbc.snb.datagen.serializer.snb.csv.staticserializer.CsvBasicStaticSerializer >> params.ini
+
+        ./run.sh
+        cp params.ini social_network/
+        # drop data files, we only need the update streams
+        rm -rf social_network/{static,dynamic}
+        mv social_network/ social_network-sf${SF}-numpart-${NUMPART}/
+    done
+
 done
